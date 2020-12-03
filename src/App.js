64c998f5bb698500ca7ad902cea50f0a435ae93d
Philip Viningston <philip.viningston@tech9.com>
2020-12-03 17:48:02 +0530

    Combined landing and login pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Grid, ThemeProvider, Container } from '@material-ui/core';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Navbar from './components/common/navbar/navbar';
+import Main from './components/common/main/main';
+import { theme } from './theme';
+import Login from './components/login/login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeProvider theme={theme}>
+        <Grid container spacing={2} key='main-outer'>
+          <Grid item sm={12} md={12} lg={12} xs={12} key='main-inner'>
+            <Container>
+              <Router>
+                <Navbar />
+                <Switch>
+                  <Route path='/login'>
+                    <Login />
+                  </Route>
+                  <Route path='/' >
+                    <Main />
+                  </Route>
+                </Switch>
+              </Router>
+            </Container>
+          </Grid>
+        </Grid>
+      </ThemeProvider>
     </div>
   );
 }
